bc940360546b20844cf0e6f2427c6e665e2ab5a9
kapcruz <karina.pereira@feedz.com.br>
2025-06-15 21:21:49 -0300

    feat: create model and config api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,3 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/reverse-me', function (Request $request) {
+    $reversed = strrev($request->input('reverse_this'));
+    return $reversed;
+});

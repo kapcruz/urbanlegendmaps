b5e0eca8524a91e1ab46cb5812c0e692d3e7136a
kapcruz <karina.pereira@feedz.com.br>
2025-10-15 19:28:40 -0300

    refactor: apply transaction atomics in the Service Layer (create,update,delete)

--- backend/app/Services/UrbanLegendService.php
+++ backend/app/Services/UrbanLegendService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\UrbanLegend;
 use App\Models\User;
 use App\Services\Interfaces\UrbanLegendServiceInterface;
@@ -25,20 +26,31 @@ class UrbanLegendService implements UrbanLegendServiceInterface
 
     public function create(array $data)
     {
-        $data['user_id'] = $this->userModel->first()->id;
-        return $this->model->create($data);
+        $data['user_id'] = $this->userModel->firstOrFail()->id;
+
+        return DB::transaction(function () use ($data) {
+            return $this->model->create($data);
+        });
     }
 
     public function update(string $uuid, array $data)
     {
         $legend = $this->find($uuid);
-        $legend->update($data);
+
+        DB::transaction(function () use ($legend, $data) {
+            $legend->update($data);
+        });
+
         return $legend;
     }
 
     public function delete(string $uuid): bool
     {
-        $legend = $this->find($uuid);
-        return (bool) $legend->delete();
+        $legend = $this->find($uuid); 
+        
+        return (bool) DB::transaction(function () use ($legend) {
+            $legend->images()->delete();
+            return $legend->delete();
+        });
     }
 }
